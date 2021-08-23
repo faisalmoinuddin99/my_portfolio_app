7b2ffa31bbd19d066bf9d566079b241b016921cd
faisal <faisal25march99@gmail.com>
2021-08-23 20:27:03 +0530

    Done with about page

--- production-webapp-using_reactjs/my-portfolio/src/App.js
+++ production-webapp-using_reactjs/my-portfolio/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Card from './components/Card'
 import DarkMode from './components/DarkMode'
-
+import profile from "../src/components/images/profile.jpg"
+import naruto from "../src/components/images/naruto.jpg"
 
 
 export default function App() {
   return (
-    <div>
-      <DarkMode />
-      <Card designation={"Software Developer"} myName={"Faisal Moinuddin"} />
 
-    </div>
+    <>
+      <div>
+        <DarkMode />
+        <Card designation={"Software Developer"} myName={"Faisal Moinuddin"}
+          imageUri={profile}
+        />
+
+      </div>
+      <br />
+      <div className="container">
+        <Card designation={"Software Tester"} myName={"Naruto Uzamaki"}
+          imageUri={naruto}
+        />
+      </div>
+    </>
   )
 }
--- production-webapp-using_reactjs/my-portfolio/src/components/Card.js
+++ production-webapp-using_reactjs/my-portfolio/src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './CardStyle.css'
-import profile from './images/profile.jpg'
+// import profile from './images/profile.jpg'
 
 export default function Card(props) {
     return (
         <div className="main-content">
             <div className="card">
-                <img src={profile} alt="faisal" />
+                <img src={props.imageUri} alt="faisal" />
                 <h2 className="primary">{props.myName}</h2>
                 <p>{props.designation}</p>
                 <button className="btn">About</button>
